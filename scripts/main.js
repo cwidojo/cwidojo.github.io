15f05a33acc859e6911d9815e69e0653e764ede5
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2014-07-23 14:29:05 -0300

    (add): simplificando script, adicionando âncoras e alterando o título da página.

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,5 @@
-var MAIN = {
-
-	containers : {},
-
-	init : function() {
-
-		MAIN.initLoadDynamicContainers();
-		MAIN.initHeaderAnimation();
-
-	},
-
-	initLoadDynamicContainers : function() {
-
-		MAIN.containers.body = $('.site-page');
-
-	},
-
-	initHeaderAnimation : function() {
-
-		MAIN.containers.body.addClass("init-animate-header");
-
-	}
-	
-};
-
 $(function() {
 	
-	MAIN.init();
+	$('.site-page').addClass("init-animate-header");
 
 }); 
